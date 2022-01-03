62510b1bb7305d5be9c3c290a78e293be0dcba0b
DevShaded <devshaded@devshaded.com>
2022-01-03 21:35:23 +0100

    Updated styles on these components to mach the color scheme of the website

--- resources/js/Jetstream/NavLink.vue
+++ resources/js/Jetstream/NavLink.vue
@@ -17,8 +17,8 @@
         computed: {
             classes() {
                 return this.active
-                    ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition'
-                    : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition'
+                    ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 font-medium leading-5 text-gray-200 focus:outline-none focus:border-indigo-700 transition'
+                    : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-white font-medium leading-5 hover:text-gray-200 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition'
             }
         }
     })
