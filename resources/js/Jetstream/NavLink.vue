3d813717a038ec6103d82fe62cf036ce39a837aa
DevShaded <devshaded@devshaded.com>
2022-01-03 22:03:00 +0100

    Fixed css issue when element is focused

--- resources/js/Jetstream/NavLink.vue
+++ resources/js/Jetstream/NavLink.vue
@@ -18,7 +18,7 @@
             classes() {
                 return this.active
                     ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 font-medium leading-5 text-gray-200 focus:outline-none focus:border-indigo-700 transition'
-                    : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-white font-medium leading-5 hover:text-gray-200 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition'
+                    : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-white font-medium leading-5 hover:text-gray-200 hover:border-gray-300 focus:outline-none focus:text-gray-400 focus:border-gray-300 transition'
             }
         }
     })
--- resources/js/Jetstream/ResponsiveNavLink.vue
+++ resources/js/Jetstream/ResponsiveNavLink.vue
@@ -24,7 +24,7 @@
             classes() {
                 return this.active
                     ? 'block pl-3 pr-4 py-2 border-l-4 border-indigo-400 text-base font-medium text-gray-200 bg-indigo-800 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition'
-                    : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-300 hover:text-white hover:bg-indigo-900 hover:border-indigo-600 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition'
+                    : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-300 hover:text-white hover:bg-indigo-900 hover:border-indigo-600 focus:outline-none focus:text-gray-300 focus:bg-indigo-900 focus:border-indigo-600 transition'
             }
         }
     })
