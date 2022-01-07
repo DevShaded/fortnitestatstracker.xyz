1c2b61451735eabfefb5647bb2ed1c3e7ff0c5be
DevShaded <devshaded@devshaded.com>
2022-01-07 21:46:42 +0100

    Changed domain name from fortnitetracker.xyz to fortnitestatstracker.xyz

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -69,7 +69,7 @@
                     </div>
                 </nav>
                 <p class="mt-8 text-center text-base text-gray-300">
-                    &copy; {{ new Date().getFullYear() }}, fortnitetracker.xyz is in <strong>no way</strong> affiliated with Epic Games
+                    &copy; {{ new Date().getFullYear() }}, <strong>fortnitestatstracker.xyz</strong> is in <strong>no way</strong> affiliated with Epic Games
                 </p>
             </div>
         </footer>
